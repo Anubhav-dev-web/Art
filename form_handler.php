fc0910618e2ccfa73e6a973de80e034e3f127e4b
Anubhav <lucimgstar007@gmail.com>
2021-11-12 23:47:42 +0530

    Update form_handler.php

--- form_handler.php
+++ form_handler.php
@@ -12,7 +12,7 @@
 
      $to = "anubhavv2002@gmail.com";
      $headers = "From: $email_from \r\n"  
-     $headers .= "Reply-To: $visitor_email \r\n"
+     $headers = "Reply-To: $visitor_email \r\n"
      
      mail($to,$email_subject,$email_body,$headers);
      header("Location: index.html");
